b5023ed55708f4d1732dd700569cda3fd2a20063
mariososadi <mariososadi@gmail.com>
2016-05-02 14:53:13 -0500

    Update abuelita.rb

--- abuelita.rb
+++ abuelita.rb
@@ -1,3 +1,23 @@
+# -----------------------------------------------------------------------------------------------------------------------
+# Primer ejemplo, método sólo evalúa si se cumple una condición.
+
+def deaf_grandma(arg)
+  arg == true ? "NO, NO DESDE 1983" : "HUH?! NO TE ESCUCHO, HIJO!" 
+end
+
+puts "Saluda a tu abuelita, hijo. Recuerda que está un poco sorda. Si crees que no te escucha, ¡GRÍTALE!. Si quieres dejar de hablarle grítale un BYE TQM"
+your_arg = gets.chomp.to_s
+
+until your_arg == 'BYE TQM' do
+  a = true
+  your_arg.each_char { |x| a = false if x != x.upcase }
+  puts deaf_grandma(a)
+  your_arg = gets.chomp.to_s
+end
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Segund ejemplo, utlizando una clase y la inicializa cada vez hasta que se escriba "BYE TQM".
+
 class Grandma
 
   attr_writer :your_arg
@@ -14,13 +34,37 @@ class Grandma
   
 end
 
-
-
 puts "Saluda a tu abuelita, hijo. Recuerda que está un poco sorda. Si crees que no te escucha, ¡GRÍTALE!. Si quieres dejar de hablarle grítale un BYE TQM"
   your_arg = gets.chomp.to_s
 
-until your_arg == 'BYE TQM' do
+until if your_arg == 'BYE TQM' do
   game = Grandma.new(your_arg)
-  p game.deaf_grandma
+  puts game.deaf_grandma
+  your_arg = gets.chomp.to_s
+end
+
+# -----------------------------------------------------------------------------------------------------------------------
+# Tercer ejemplo, usando un método para toda la acción comparativa. El loop while contiene un if/else para incluir el "TQM
+#  BYE" tres veces como línea de salida.
+
+
+def deaf_grandma(your_arg)
+  a = true
+  your_arg.each_char { |x| a = false if x != x.upcase }
+  a == true ? "NO, NO DESDE 1983" : "HUH?! NO TE ESCUCHO, HIJO!" 
+end
+
+puts "Saluda a tu abuelita, hijo. Recuerda que está un poco sorda. Si crees que no te escucha, ¡GRÍTALE!. Si quieres dejar de hablarle grítale un BYE TQM, o grítaselo 3 veces"
+  your_arg = gets.chomp.to_s
+
+while if your_arg == 'BYE TQM'
+        false
+      elsif your_arg == 'BYE TQM BYE TQM BYE TQM'
+        false
+      else
+        true
+      end
+  
+  puts deaf_grandma(your_arg)
   your_arg = gets.chomp.to_s
 end
