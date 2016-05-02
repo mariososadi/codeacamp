756b4d67affe1a751dc2e0bcb073966ecc93096c
mariososadi <mariososadi@gmail.com>
2016-05-02 12:41:55 -0500

    Update elefante.rb

--- elefante.rb
+++ elefante.rb
@@ -1,22 +1,21 @@
 class Elefante
-
+	
   def intialize(num)
-	  @num = num
-	end
+    @num = num
+  end
+  
+  def loop_esp
+    1.upto(@num) { |n| p "#{n} elefante se columpiaba sobre la tela de una araña, como veía que resistía fueron a llamar a otro elefante."}
+  end
 
-	def loop_esp
-	  1.upto(@num) { |n| p "#{n} elefante se columpiaba sobre la tela de una araña, como veía que resistía fueron a llamar a otro elefante."}
-	end
-
-	def loop_eng
-		if @num == 0
-			99.downto(@num + 1) { |n| p "#{n} bottles of beer on the wall, #{n} bottles of beer. Take one down and pass it around, #{n-1} bottles of beer on the wall."}
-			"No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
-		else 
-			99.downto(@num) { |n| p "#{n} bottles of beer on the wall, #{n} bottles of beer. Take one down and pass it around, #{n-1} bottles of beer on the wall."}
-	  end
-	end
-
+  def loop_eng
+    if @num == 0
+      99.downto(@num + 1) { |n| p "#{n} bottles of beer on the wall, #{n} bottles of beer. Take one down and pass it around, #{n-1} bottles of beer on the wall."}
+      "No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall."
+    else 
+      99.downto(@num) { |n| p "#{n} bottles of beer on the wall, #{n} bottles of beer. Take one down and pass it around, #{n-1} bottles of beer on the wall."}
+    end
+  end
 end
 
 start_song = Elefante.new
